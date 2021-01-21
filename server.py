87ffbf0613bd5e4794ae0664dc996573efdec526
Alex <leha3521@gmail.com>
2021-01-21 15:58:12 +0200

    LOL KEK print in server.py

--- server.py
+++ server.py
@@ -6,9 +6,9 @@ HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 connections = []
 
-
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+print("LOL, KEK")
 server.bind((HOST, PORT))
 server.listen()
 
